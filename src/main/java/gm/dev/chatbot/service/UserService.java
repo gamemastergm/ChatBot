f894233b42e4a99f10abb57fe2ba348f233f66f1
luisfelipe-tec <luisfelipe.tecnologias@gmail.com>
2024-05-11 00:15:03 -0300

    Adicionado o método de criação do usuário nos serviços.

--- src/main/java/gm/dev/chatbot/service/UserService.java
+++ src/main/java/gm/dev/chatbot/service/UserService.java
@@ -1,8 +1,11 @@
 package gm.dev.chatbot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import gm.dev.chatbot.model.ExceptionModel;
 import gm.dev.chatbot.model.User;
 import gm.dev.chatbot.repository.UserRepository;
 
@@ -10,9 +13,29 @@ import gm.dev.chatbot.repository.UserRepository;
 public class UserService {
     
     @Autowired
+    private ExceptionModel ex;
+
+    @Autowired
     private UserRepository ur;
 
     public Iterable<User> list(){
         return ur.findAll();
     }
+
+    public ResponseEntity<?> cadastrarUser(User user){
+        if (user.getLogin().equals("")) {
+            ex.setMensagem("O login é obrigatório");
+            return new ResponseEntity<>(ex, HttpStatus.BAD_REQUEST);
+            
+        } else if(user.getPassword().equals("")) {
+            ex.setMensagem("A senha é obrigatória");
+            return new ResponseEntity<>(ex, HttpStatus.BAD_REQUEST);
+         } else if(user.getEmail().equals("")) {
+            ex.setMensagem("O email é obrigatório");
+            return new ResponseEntity<>(ex, HttpStatus.BAD_REQUEST);
+         } else {
+            return new ResponseEntity<User>(ur.save(user), HttpStatus.CREATED);
+         }
+
+}
 }

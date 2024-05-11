28529062c4b2374ca311742eb4b66a42e50579ce
luisfelipe-tec <luisfelipe.tecnologias@gmail.com>
2024-05-10 22:31:33 -0300

    Criação do model

---
+++ src/main/java/gm/dev/chatbot/model/Question.java
@@ -0,0 +1,22 @@
+package gm.dev.chatbot.model;
+
+import java.util.ArrayList;
+import com.vaadin.flow.component.template.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "question")
+@Getter
+@Setter
+
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ArrayList<String> questions;
+    private String answer;
+}

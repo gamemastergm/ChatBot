3a9fee9dc0497a61e99797bb9290125e1c240d19
luisfelipe-tec <luisfelipe.tecnologias@gmail.com>
2024-05-10 23:59:49 -0300

    Corrigindo as importações do modelo

--- src/main/java/gm/dev/chatbot/model/Question.java
+++ src/main/java/gm/dev/chatbot/model/Question.java
@@ -1,10 +1,11 @@
 package gm.dev.chatbot.model;
 
 import java.util.ArrayList;
-import com.vaadin.flow.component.template.Id;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
